9ceba50711e62a13853b8f04788d831763e8628f
Waiwhakaira Falwasser <waifalcode@gmail.com>
2025-10-21 10:55:29 +1300

    Fetch and inject template using `XMLHttpRequest`

--- src/app.js
+++ src/app.js
@@ -1,4 +1,31 @@
+"use strict";
+
 // Web Components
 import './components/Navbar.js';        // Navbar
 import './components/Header.js';        // Header
 import './components/Card.js';          // Card
+
+document.addEventListener('DOMContentLoaded', function () {
+    const xhr = new XMLHttpRequest();
+
+    xhr.onload = function () {
+        if (xhr.status !== 200) {
+            console.error(`Failed to fetch template: ${xhr.statusText}`);
+            return;
+        }
+
+        try {
+            const data = xhr.responseText;
+            document.getElementById('app').innerHTML = data;
+        } catch (error) {
+            console.error('Failed to initialize template: ', error);
+        }
+    }
+    
+    xhr.onerror = function () {
+        console.error('Network error or failed to fetch template.');
+    };
+
+    xhr.open('GET', '/src/template/home.html', true);
+    xhr.send(null);
+});
